ea0ec2e08e2aeccfef1d7e35ffbd8d1b14e3fc82
typeofMax <work@typeofmax.ru>
2023-02-12 21:24:16 +0300

    Complete task 3

--- task_3.js
+++ task_3.js
@@ -1,9 +1,10 @@
 function createEmptyObject() {
-    const obj = Object.create(null);
-
-    return obj;
+    return Object.create(null);
 }
 
 const user = createEmptyObject();
+
+user.name = 'Max';
+user.email = 'test@example.com';
 
 console.log(user);
